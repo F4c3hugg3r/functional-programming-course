5d86796820993e04d01fd43b4674ad36f456bb60
F4c3hugg3r <l.dubhorn@gmail.com>
2024-02-27 17:27:46 +0100

    Consumer

---
+++ src/main/java/org/example/_Consumer.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class _Consumer {
+    public static void main(String[] args) {
+        timesTenConsumer.accept(1);
+        incrementPlusCommentConsumer.accept(1, "Super gemacht");
+    }
+
+    static Consumer<Integer> timesTenConsumer = number -> System.out.println(number*10);
+    static BiConsumer<Integer, String> incrementPlusCommentConsumer = (number, comment) ->
+            System.out.println((number+1)+" Kommentar: "+comment);
+
+}
